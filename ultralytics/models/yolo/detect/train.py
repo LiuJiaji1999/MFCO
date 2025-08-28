ea39cd137a95d80391ced30ed114ee0d503ca370
LiuJiaji1999 <494882733@qq.com>
2025-08-28 19:58:23 +0800

    add

--- ultralytics/models/yolo/detect/train.py
+++ ultralytics/models/yolo/detect/train.py
@@ -96,7 +96,6 @@ class DetectionTrainer(BaseTrainer):
         """Returns a DetectionValidator for YOLO model validation."""
         # self.loss_names = "box_loss", "cls_loss", "dfl_loss"
         self.loss_names = "box_loss", "cls_loss", "dfl_loss","gob_loss","loc_loss"  
-       
         return yolo.detect.DetectionValidator(
             self.test_loader, save_dir=self.save_dir, args=copy(self.args), _callbacks=self.callbacks
         )
