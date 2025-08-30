97cdbc28ddbb31f4f98c172033daed246a6a765c
LiuJiaji1999 <494882733@qq.com>
2025-08-30 16:26:39 +0800

    run-vis

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/11/yolo11m.yaml') # YOLO11
     # model = YOLO('/home/lenovo/data/liujiaji/ultralytics-yolo11-main/runs/train/exp4/weights/last.pt') # YOLO11
     model.load('yolo11m.pt') # loading pretrain weights
-    model.train(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/publicallpower.yaml',
+    model.train(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/VisDrone.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=100,
@@ -25,3 +25,23 @@ if __name__ == '__main__':
                 project='runs/train',
                 name='exp',
                 )
+    # ## seg
+    # model = YOLO('ultralytics/cfg/models/11/yolo11m-seg.yaml') # YOLO11
+    # model.load('yolo11m-seg.pt') # loading pretrain weights
+    # model.train(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/cod10k.yaml',
+    #             cache=False,
+    #             imgsz=640,
+    #             epochs=100,
+    #             batch=2, # baseline=4
+    #             close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
+    #             workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
+    #             # device='0,1', # 指定显卡和多卡训练
+    #             optimizer='SGD', # using SGD
+    #             patience=0, # set 0 to close earlystop.
+    #             resume=True, # 断点续训,YOLO初始化时选择last.pt
+    #             # amp=False, # close amp | loss出现nan可以关闭amp
+    #             # cos_lr = True,
+    #             # fraction=0.2,
+    #             project='runs/seg',
+    #             name='exp',
+    #             )
--- val.py
+++ val.py
@@ -17,9 +17,9 @@ def get_weight_size(path):
     return f'{stats.st_size / 1024 / 1024:.1f}'
 
 if __name__ == '__main__':
-    model_path = 'runs/train/exp4/weights/best.pt'
+    model_path = 'runs/train/exp5/weights/best.pt'
     model = YOLO(model_path) # 选择训练好的权重路径
-    result = model.val(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/powerdata.yaml',
+    result = model.val(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/publicallpower.yaml',
                         split='val', # split可以选择train、val、test 根据自己的数据集情况来选择.
                         imgsz=640,
                         batch=4,
