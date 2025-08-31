489f5b6274aa35453cef3dccb933b0a5e8e53cea
LiuJiaji1999 <494882733@qq.com>
2025-08-31 09:31:03 +0800

    runvis

--- segtoyolo.py
+++ segtoyolo.py
@@ -1,6 +1,6 @@
 from ultralytics.data.converter import convert_segment_masks_to_yolo_seg
 
 # The classes here is the total classes in the dataset, for COCO dataset we have 80 classes
-convert_segment_masks_to_yolo_seg("/home/lenovo/data/liujiaji/Datasets-Multiview/NC4K/GT", 
-                                  "/home/lenovo/data/liujiaji/Datasets-Multiview/NC4K/txt",
-                                    classes=80)
+convert_segment_masks_to_yolo_seg("/home/lenovo/data/liujiaji/Datasets-Multiview/COD10K-v3/Test/GT_Object", 
+                                  "/home/lenovo/data/liujiaji/Datasets-Multiview/COD10K-v3/txt",
+                                    classes=69)
