96119d180c8f3fc521f4cedb0ae40abeb6526e93
LiuJiaji1999 <494882733@qq.com>
2025-09-05 15:31:38 +0800

    detect

--- detect.py
+++ detect.py
@@ -4,17 +4,17 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('runs/train/exp/weights/best.pt') # select your model.pt path
-    model.predict(source='dataset/images/test',
+    model = YOLO('runs/train/exp4/weights/best.pt') # select your model.pt path
+    model.predict(source='/home/lenovo/data/liujiaji/powerGit/yolov8/testImg',
                   imgsz=640,
                   project='runs/detect',
-                  name='exp',
+                  name='exp4',
                   save=True,
-                  # conf=0.2,
+                  conf=0.3,
                   # iou=0.7,
                   # agnostic_nms=True,
                   # visualize=True, # visualize model features maps
-                  # line_width=2, # line width of the bounding boxes
+                  line_width=5, # line width of the bounding boxes
                   # show_conf=False, # do not show prediction confidence
                   # show_labels=False, # do not show prediction labels
                   # save_txt=True, # save results as .txt file
