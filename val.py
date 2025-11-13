a57b882315fb9415aec87f216541e355dff73315
LiuJiaji1999 <494882733@qq.com>
2025-11-13 14:08:35 +0800

    add

--- val.py
+++ val.py
@@ -12,11 +12,11 @@ def get_weight_size(path):
     return f'{stats.st_size / 1024 / 1024:.1f}'
 
 if __name__ == '__main__':
-    model_path = 'runs/train/exp27/weights/best.pt' # 测试结果都为0的话，用fp32的
+    model_path = 'runs/train/exp17/weights/best_fp32.pt' # 测试结果都为0的话，用fp32的
     model = YOLO(model_path) # 选择训练好的权重路径
     result = model.val(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/powerdata.yaml',
                         split='test', # split可以选择train、val、test 根据自己的数据集情况来选择.
-                        imgsz=640,
+                        imgsz=800,
                         batch=4, # 该值对结果的影响不大
                         # iou=0.7,
                         # rect=False,
